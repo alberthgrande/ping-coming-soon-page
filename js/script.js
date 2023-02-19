f687a42632714495faec4d479c9fe7402a491557
Alberth G. Ruado <alberthruado@gmail.com>
2023-02-19 22:21:44 +0800

    Done Challenge

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-const emailInput = document.querySelector("input").value;
+const emailInput = document.querySelector("input");
 const errorMsg = document.querySelector("small");
+
+let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 function formValidation() {
-  errorMsg.innerHTML = "Please provide valid email address";
+  if (emailInput.value === "") {
+    errorMsg.innerHTML = `Email cannot be empty`;
+    emailInput.style.borderColor = "hsla(0, 100%, 74%, 0.7)";
+    emailInput.style.borderWidth = "2px";
+    emailInput.style.borderStyle = "solid";
+  } else if (emailInput.value.match(mailformat)) {
+    errorMsg.style.display = `none`;
+    emailInput.style.borderColor = "hsla(154, 59%, 51%,0.7)";
+    emailInput.style.borderWidth = "2px";
+    emailInput.style.borderStyle = "solid";
+  } else {
+    errorMsg.innerHTML = `Please provide valid email address`;
+    emailInput.style.borderColor = "hsla(0, 100%, 74%, 0.7)";
+    emailInput.style.borderWidth = "2px";
+    emailInput.style.borderStyle = "solid";
+  }
 }
